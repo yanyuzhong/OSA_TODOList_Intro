64cd2a8281b19138d38735c755d452226ba8291b
Yanyu Zhong <yz133@rice.edu>
2020-05-28 23:07:45 -0500

    add textfield and material-ui

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TextField from "@material-ui/core/TextField";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>TODO List</div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form className={App.root} noValidate autoComplete="off">
+          <TextField
+            id="outlined-basic"
+            label="Enter Todo Item"
+            variant="outlined"
+          />
+        </form>
         <a
           className="App-link"
           href="https://reactjs.org"
